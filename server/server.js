ed7205b76d0ba0e1821540d239f02bebcad7d7aa
swendt57 <stevewendt@cox.net>
2018-12-12 17:32:41 -0800

    GET by ID via Postman

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -9,28 +11,51 @@ var  app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    // console.log("Hello");
-    // console.log(req.body);
+// app.post('/todos', (req, res) => {
+//     // console.log("Hello");
+//     // console.log(req.body);
+//
+//     var todo = new Todo({
+//         text: req.body.text
+//     });
+//
+//     todo.save().then( (doc) => {
+//         res.send(doc);
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+// });
+//
+// app.get('/todos', (req, res) => {
+//     Todo.find().then((todos) => {
+//         res.send({todos})
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+// });
 
-    var todo = new Todo({
-        text: req.body.text
-    });
+//GET by passing in parameter
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
 
-    todo.save().then( (doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
+    if( ! ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if( ! todo) {
+            res.status(404).send({});
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send({}))
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({todos})
-    }, (e) => {
-        res.status(400).send(e);
-    });
-});
+// User.findById(userID).then((user) => {
+//     if( ! user ) {
+//         return console.log("User ID not found");
+//     }
+//     console.log('User by ID:', user);
+// }).catch((e) => {console.log(e)});
 
 app.listen(3000, () => {
     console.log('started on port 3000');
