25a7bebec5198d4f17ac4854640e24e45d0bd458
swendt57 <Lthr4ever>
2018-12-19 13:39:36 -0800

    added in support for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var  app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -57,8 +58,8 @@ app.get('/todos/:id', (req, res) => {
 //     console.log('User by ID:', user);
 // }).catch((e) => {console.log(e)});
 
-app.listen(3000, () => {
-    console.log('started on port 3000');
+app.listen(port, () => {
+    console.log(`started on port ${port}`);
 });
 
 module.exports = {app};
