c425fa72b7747250437accd844e43e6771256f30
swendt57 <Lthr4ever>
2018-12-19 09:19:07 -0800

    added in test cases for todo GET by ID

--- server/server.js
+++ server/server.js
@@ -11,28 +11,28 @@ var  app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {
-//     // console.log("Hello");
-//     // console.log(req.body);
-//
-//     var todo = new Todo({
-//         text: req.body.text
-//     });
-//
-//     todo.save().then( (doc) => {
-//         res.send(doc);
-//     }, (e) => {
-//         res.status(400).send(e);
-//     });
-// });
-//
-// app.get('/todos', (req, res) => {
-//     Todo.find().then((todos) => {
-//         res.send({todos})
-//     }, (e) => {
-//         res.status(400).send(e);
-//     });
-// });
+app.post('/todos', (req, res) => {
+    // console.log("Hello");
+    // console.log(req.body);
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then( (doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 //GET by passing in parameter
 app.get('/todos/:id', (req, res) => {
