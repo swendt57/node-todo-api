f44b628810f0c5439e194b4cb79863d140092fa9
swendt57 <Lthr4ever>
2018-12-19 19:39:48 -0800

    added PATCH (UPDATE) functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -12,23 +12,6 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-//**************REMOVE EXAMPLES********************
-
-app.delete('/todos/:id', (req, res) => {
-    let id = req.params.id;
-
-    if( ! ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
-
-    Todo.findByIdAndRemove(id).then((todo) => {
-        if( ! todo) {
-           return res.status(404).send();
-        }
-        res.send({todo});
-
-    }).catch((e) => res.status(400).send())
-});
 
 //**************POST EXAMPLES*********************
 
@@ -79,6 +62,52 @@ app.get('/todos/:id', (req, res) => {
 //     }
 //     console.log('User by ID:', user);
 // }).catch((e) => {console.log(e)});
+
+//**************REMOVE EXAMPLES********************
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if( ! ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if( ! todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+
+    }).catch((e) => res.status(400).send())
+});
+
+//************UPDATE EXAMPLES**********************
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if( ! ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if( ! todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo: todo});  //can also use just {todo} in this case
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
 
 app.listen(port, () => {
     console.log(`started on port ${port}`);
