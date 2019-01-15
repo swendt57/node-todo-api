1321152966aba49560d4c74c7b9cdf1ecb6e77e8
swendt57 <Lthr4ever>
2019-01-14 17:29:18 -0800

    updated config to use JSON files

--- server/models/user.js
+++ server/models/user.js
@@ -48,7 +48,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function () {
     let user = this;
     let access = 'auth';
-    let token = jwt.sign({_id: user._id.toHexString(), access}, '123abc').toString();
+    let token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens = user.tokens.concat([{access, token}]); //Update from class
 
@@ -74,7 +74,7 @@ UserSchema.statics.findByToken = function(token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, '123abc');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     }catch(e) {
         return Promise.reject(); //shorthand
